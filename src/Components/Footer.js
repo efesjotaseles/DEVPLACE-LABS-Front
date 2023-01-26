cad7ab1a273ec1c68d1ad485c1fae72a6244009c
FacuMangi <facumangioni@gmail.com>
2023-01-26 13:25:21 -0300

    Nuevo front

--- src/Components/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-    const style = {backgroundColor:"darkblue", position: "absolute", bottom: 0, width: "100%", height: "6vh"}
-  return (
-    <div style={style} >footer</div>
-  )
-}
