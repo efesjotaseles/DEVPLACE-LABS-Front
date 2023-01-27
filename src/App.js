5b6ef50bb02d1db45fe25d9b0417d60c147950f7
FacuMangi <facumangioni@gmail.com>
2023-01-27 01:28:17 -0300

    Combinado el header de jhonatan con la app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import Profile from "./pages/profile/Profile";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { NavbarLayout } from './Components/Navbar';
+import { PublicationForm } from "./Pages/Profile/PublicationForm";
+import { ProfilePage } from './Pages/Profile/ProfilePage';
+import Login from './Pages/Login/LoginComponents/login';
+import SignUp from './Pages/Login/LoginComponents/singup';
 
 function App() {
-  return <Profile/>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<NavbarLayout/>}>
+            <Route path="/publication" element={<PublicationForm/>}/>
+            <Route path="/profile" element={<ProfilePage/>}/>
+            <Route path="/sign-in" element={<Login />} />
+            <Route path="/sign-up" element={<SignUp />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/PostList.js
@@ -0,0 +1,40 @@
+export function PostList(){
+
+    let handleUpdate = () => {
+        return (
+            <></>
+        )
+    }
+    let handleDelete = () => {
+        return (
+            <></>
+        )
+    }
+
+    return (
+        <table className="table mb-3">
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Title</th>
+          <th>Views</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+                <td>19/3/23</td>
+                <td>Primer Post</td>
+                <td>5</td>
+                <td>
+                    <div className="mb-3">
+                        <button onClick={() => handleUpdate()} className="btn btn-dark">Update</button>
+                    </div>
+                    <div className="mb-3">
+                        <button onClick={() => handleDelete()} className="btn btn-danger">Delete</button>
+                    </div>
+                </td>
+            </tr>
+      </tbody>
+    </table>
+    )
+}
