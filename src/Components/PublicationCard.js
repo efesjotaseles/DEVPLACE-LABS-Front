cdc92132b4967757fd16698c1e7b6e183f0e7b9e
Fede <efesjotaseles2@gmail.com>
2023-01-30 23:13:43 -0300

    layout básico de card terminado y fix de bug en like

--- src/Components/PublicationCard.js
+++ src/Components/PublicationCard.js
@@ -1,79 +1,105 @@
 import { useEffect, useState } from "react";
-import { ButtonGroup, ToggleButton } from "react-bootstrap";
+import { Col, Container, Row, ToggleButton } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 
 function PublicationCard() {
-  const cardText = "ACÁ VA EL TEXTO DE LA PUBLICACIÓN";
+  
+  //useStates
+  const [content, setContent] = useState();
+  const [user, setUser] = useState();
+  const [likeState, setLikeState] = useState(false);
+  const [dislikeState, setDislikeState] = useState(false);
   const [favedState, setFavedState] = useState(false);
-  const [likeValue, setLikeValue] = useState(0);
-  const [likeRadioChecked, setLikeRadioChecked] = useState(false);
 
-  const likeRadios = [
-    {
-      name: "Like",
-      value: 1,
-      variant: "outline-success",
-    },
-    { name: "Dislike", value: 2, variant: "outline-danger" },
-  ];
   useEffect(() => {
-    console.log(`Initial values: ${likeValue} | ${likeRadioChecked}`);
-  }, []);
+    setContent("Contenido de la publicación...");
+    setUser("fulanito");
+  });
 
-  const handleFavChange = () => {
+  //Subcomponents
+
+  function LikeButton() {
+    return (
+      <ToggleButton
+        id="like-bttn"
+        type="checkbox"
+        variant={likeState ? "success" : "outline-success"}
+        checked={likeState}
+        onChange={handleLikeChange}
+      >
+        Like
+      </ToggleButton>
+    );
+  }
+
+  function DislikeButton() {
+    return (
+      <ToggleButton
+        id="dislike-bttn"
+        type="checkbox"
+        variant={dislikeState ? "danger" : "outline-danger"}
+        checked={dislikeState}
+        onChange={handleDislikeChange}
+      >
+        Dislike
+      </ToggleButton>
+    );
+  }
+
+  function FavButton() {
+    return (
+      <ToggleButton
+        id="fav-check"
+        type="checkbox"
+        variant={favedState ? "warning" : "outline-warning"}
+        checked={favedState}
+        onChange={handleFavChange}
+      >
+        Fav!
+      </ToggleButton>
+    );
+  }
+
+  //Handles
+
+  function handleLikeChange() {
+    setLikeState(!likeState);
+    if (dislikeState) {
+      setDislikeState(false);
+    }
+  }
+
+  function handleDislikeChange() {
+    setDislikeState(!dislikeState);
+    if (likeState) {
+      setLikeState(false);
+    }
+  }
+
+  function handleFavChange() {
     setFavedState(!favedState);
     //TODO endpoint with backend
-  };
-
-  //TODO fix to uncheck button on change
-  const handleLikeRadioChange = (e) => {
-    const value = e.currentTarget.value;
-    console.log(`Values before if: ${likeValue} | ${likeRadioChecked}`);
-    if (value !== likeValue) {
-      setLikeValue(value);
-      setLikeRadioChecked(true);
-      console.log(`Values changes when value !== likeValue: ${likeValue} | ${likeRadioChecked}`);
-    } else {
-      setLikeValue(0);
-      setLikeRadioChecked(false);
-      console.log(`Values changes when value === likeValue: ${likeValue} | ${likeRadioChecked}`);
-    }
-  };
+  }
 
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Body>
-        <Card.Text>{cardText}</Card.Text>
-        <ButtonGroup>
-          {likeRadios.map((likeRadio, idx) => {
-            return (
-              <ToggleButton
-                key={idx}
-                id={`likeRadio-${idx}`}
-                type="radio"
-                variant={likeRadio.variant}
-                name="likeRadio"
-                onClick={(e) => {
-                  handleLikeRadioChange(e);
-                }}
-                value={likeRadio.value}
-                checked={likeRadioChecked}
-              >
-                {likeRadio.name}
-              </ToggleButton>
-            );
-          })}
-        </ButtonGroup>
-        <ToggleButton
-          id="fav-check"
-          type="checkbox"
-          variant={favedState ? "warning" : "outline-warning"}
-          checked={favedState}
-          onChange={handleFavChange}
-        >
-          Fav!
-        </ToggleButton>
+    <Card border="primary" bg="light" style={{ width: "40%" }}>
+      <Card.Body style={{ minHeight: "20vh" }}>
+        <Card.Text>{content}</Card.Text>
       </Card.Body>
+      <Card.Footer>
+        <Container>
+          <Row>
+            <Col>
+              <Card.Text className="text-muted">{user}</Card.Text>
+            </Col>
+            <Col>
+              <LikeButton />
+              <DislikeButton />
+              <FavButton />
+            </Col>
+          </Row>
+        </Container>
+      </Card.Footer>
     </Card>
   );
 }
