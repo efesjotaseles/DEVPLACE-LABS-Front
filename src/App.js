0113d6c907a9a95a8c41da3835fb1fef1f1cfd84
Fede <efesjotaseles2@gmail.com>
2023-02-01 12:45:58 -0300

    Finished fav funcionality on PublicationCard

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { NavbarLayout } from './Components/Navbar';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { NavbarLayout } from "./Components/Navbar";
 import { PublicationForm } from "./Pages/PublicationForm";
-import { ProfilePage } from './Pages/ProfilePage';
-import PublicationCard from './Components/PublicationCard';
+import { ProfilePage } from "./Pages/ProfilePage";
+import PublicationCard from "./Components/PublicationCard";
+import axios from "axios";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<PublicationCard/>}>
+          <Route
+            path="/"
+            element={
+              <PublicationCard
+                publication={{
+                  content: "Contenido pasado por props!",
+                  userId: "63d29efe9fdab0e31b2d7b0d",
+                  id: "63c99d1d62b6db5b8ad22fbc",
+                  favedBy: ["10101", "4444", "63d29efe9fdab0e31b2d7b0d"],
+                }}
+                requestingUser={{ userId: "63d29efe9fdab0e31b2d7b0d" }}
+              />
+            }
+          >
             {/* <Route path="/publication" element={<PublicationForm/>}/>
             <Route path="/profile" element={<ProfilePage/>}/> */}
           </Route>
--- src/Components/PublicationCard.js
+++ src/Components/PublicationCard.js
@@ -1,20 +1,43 @@
 import { useEffect, useState } from "react";
 import { Col, Container, Row, ToggleButton } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
+import axios from "axios";
 
-function PublicationCard() {
-  
+const apiUsers = axios.create({ baseURL: "http://localhost:3030/users/" });
+const apiPublications = axios.create({
+  baseURL: "http://localhost:3030/publications/",
+});
+
+function PublicationCard(props) {
   //useStates
-  const [content, setContent] = useState();
-  const [user, setUser] = useState();
+  const [publication, setPublication] = useState({});
+  const [publicationUser, setPublicationUser] = useState();
+
   const [likeState, setLikeState] = useState(false);
   const [dislikeState, setDislikeState] = useState(false);
   const [favedState, setFavedState] = useState(false);
 
+  //onMount
   useEffect(() => {
-    setContent("Contenido de la publicación...");
-    setUser("fulanito");
-  });
+    setPublication(props.publication);
+    getUserName(props.publication.userId);
+    //Chequeamos si userId hizo fav en la publicación
+    props.publication.favedBy.includes(props.requestingUser.userId)
+      ? setFavedState(true)
+      : setFavedState(false);
+    //TODO chequear like/dislike
+  }, []);
+
+  function getUserName(id) {
+    apiUsers
+      .get(id)
+      .then((response) => {
+        setPublicationUser(response.data.name);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   //Subcomponents
 
@@ -78,19 +101,21 @@ function PublicationCard() {
 
   function handleFavChange() {
     setFavedState(!favedState);
-    //TODO endpoint with backend
+    apiPublications.put(
+      `id/${publication.id}/userId/${props.requestingUser.userId}`
+    );
   }
 
   return (
     <Card border="primary" bg="light" style={{ width: "40%" }}>
       <Card.Body style={{ minHeight: "20vh" }}>
-        <Card.Text>{content}</Card.Text>
+        <Card.Text>{publication.content}</Card.Text>
       </Card.Body>
       <Card.Footer>
         <Container>
           <Row>
             <Col>
-              <Card.Text className="text-muted">{user}</Card.Text>
+              <Card.Text className="text-muted">{publicationUser}</Card.Text>
             </Col>
             <Col>
               <LikeButton />
